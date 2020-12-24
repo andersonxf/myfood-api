e0b89df84447b110fd1afa76ff7092eedc1305bb
Anderson Fernandes <anderson.fernandes@everis.com>
2020-12-24 16:54:43 -0300

    diversas mais formaPagamento

---
+++ src/main/java/com/andersonxf/myfood/api/assembler/FormaPagamentoInputDisassembler.java
@@ -0,0 +1,23 @@
+package com.andersonxf.myfood.api.assembler;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.andersonxf.myfood.api.model.input.FormaPagamentoInput;
+import com.andersonxf.myfood.domain.model.FormaPagamento;
+
+@Component
+public class FormaPagamentoInputDisassembler {
+
+    @Autowired
+    private ModelMapper modelMapper;
+    
+    public FormaPagamento toDomainObject(FormaPagamentoInput formaPagamentoInput) {
+        return modelMapper.map(formaPagamentoInput, FormaPagamento.class);
+    }
+    
+    public void copyToDomainObject(FormaPagamentoInput formaPagamentoInput, FormaPagamento formaPagamento) {
+        modelMapper.map(formaPagamentoInput, formaPagamento);
+    }   
+}        
